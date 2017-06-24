e57f111360e1caea6ddc9ddc9ab4153ae73cb0d1
15123906906 <807163565@qq.com>
2017-06-25 00:49:55 +0800

    new_API

--- My_DOM_API.js
+++ My_DOM_API.js
@@ -18,3 +18,19 @@ function insertAfter(newElement,targetElement){
 		parent.insertBefore(newElement,targetElement.nextSibling);
 	}
 }
+
+function getHTTPObject(){
+	//获取XMLHttpRequest对象
+	if(typeof XMLHttpRequest == "undefined"){
+		XMLHttpRequest = function(){
+			try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+			catch (e){}
+			try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+			catch (e){}
+			try { return new ActiveXObject("Msxml2.XMLHTTP"); }
+			catch (e){}
+			return false;
+		}
+	}
+	return new XMLHttpRequest();
+}
